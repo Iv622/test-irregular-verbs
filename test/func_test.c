7b3422078decbc30e9557c93b4a993107ad8bebb
varnas98 <varnas98@mail.ru>
2017-05-30 10:44:02 +0600

    Fix_test

--- test/func_test.c
+++ test/func_test.c
@@ -1,102 +1,66 @@
-
 #include "ctest.h"
 #include <func.h>
 
 CTEST(test_result_five, test_printf)
 {
-   
-    const int t = 19;
-
-  
-    const int result = result_print(t);
-
-  
-    const int expected = 5;
-    ASSERT_EQUAL(expected, result);
+	const int tested_value = 19;
+	сonst int result = result_print(tested_value);
+	const int expected_result= 5;
+	ASSERT_EQUAL(expected_result, result);
 }
 
 CTEST(test_result_four, test_printf)
 {
-   
-    const int t = 17;
-
-  
-    const int result = result_print(t);
-
-  
-    const int expected = 4;
-    ASSERT_EQUAL(expected, result);
+	const int tested_value = 17;
+	const int result = result_print(tested_value);
+	const int expected_result = 4;
+	ASSERT_EQUAL(expected_result, result);
 }
- 
+
 CTEST(test_result_three, test_printf)
 {
-   
-    const int t = 12;
-
-  
-    const int result = result_print(t);
-
-  
-    const int expected = 3;
-    ASSERT_EQUAL(expected, result);
+	const int tested_value = 12;
+	const int result = result_print(tested_value);
+	const int expected_result = 3;
+	ASSERT_EQUAL(expected_result, result);
 }
 
 CTEST(test_result_two, test_printf)
 {
-   
-    const int t = 8;
-
-  
-    const int result = result_print(t);
-
-  
-    const int expected = 2;
-    ASSERT_EQUAL(expected, result);
+	const int tested_value = 8;
+	const int result = result_print(tested_value);
+	const int expected_result = 2;
+	ASSERT_EQUAL(expected_result, result);
 }
+
 CTEST(test_result_min0, test_printf)
 {
-   
-    const int t = -1;
-
-  
-    const int result = result_print(t);
-
-  
-    const int expected = 0;
-    ASSERT_EQUAL(expected, result);
+	const int tested_value = -1;
+	const int result = result_print(tested_value);
+	const int expected_result = 0;
+	ASSERT_EQUAL(expected_result, result);
 }
+
 CTEST(test_result_max0, test_printf)
 {
-   
-    const int t = 21;
-
-  
-    const int result = result_print(t);
-
-  
-    const int expected = 0;
-    ASSERT_EQUAL(expected, result);
+	const int tested_value = 21;
+	const int result = result_print(tested_value);
+	const int expected_result = 0;
+	ASSERT_EQUAL(expected_result, result);
 }
+
 CTEST(test_verbsform_less1, test_verbsform)
 {
-   
-    const int t =0;
+	const int tested_value =0;
+	const int result = test_verbs(tested_value);
+	const int expected_result= 0;
+	ASSERT_EQUAL(expected_result, result);
+}
 
-  
-    const int result = test_verbs(t);
-
-  
-    const int expected = -1;
-    ASSERT_EQUAL(expected, result);
-}CTEST(test_verbsform_more3, test_verbsform)
+CTEST(test_verbsform_more3, test_verbsform)
 {
-   
-    const int t = 4;
-
-  
-    const int result = test_verbs(t);
-
-  
-    const int expected = -1;
-    ASSERT_EQUAL(expected, result);
+	const int tested_value = 4;
+	const int result = test_verbs(tested_value);
+	const int expected_result = 0;
+	ASSERT_EQUAL(expected_result, result);
 }
